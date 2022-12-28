5f9d283ecda40865f1d976fe8651e1c934a96ebb
Julio Contreras <julio-alberto.aranis@soprasteria.com>
2022-12-28 09:14:47 +0100

    feat: remove logout

--- src/application/routes/auth/index.ts
+++ src/application/routes/auth/index.ts
@@ -1,11 +1,9 @@
 import { serverHTTP } from '@/adapters/serverHTTP'
 
 import { signinCaseUse } from '@/application/useCases/signin'
-import { logoutCaseUse } from '@/application/useCases/logout'
 import { registerCaseUse } from '@/application/useCases/register'
 import { isAuthenticatedCaseUse } from '@/application/useCases/isAuthenticated'
 import { resetPasswordCaseUse } from '@/application/useCases/resetPassword'
-
 
 export function useRoutesAuth () {
    
